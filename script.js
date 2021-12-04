c87145006affdc37817fd019aea962773345b153
Guylli1 <g.fernandes2223@gmail.com>
2021-12-04 17:20:43 -0300

    Comprovante

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const btn = document.getElementById('but1')
-
-btn.addEventListener('click', function () {
-  console.log('clicou')
-})
+function carConteudo1() {
+  document.getElementById('but1').innerText = 'Mês de dezembro PAGO!'
+}
+function carConteudo2() {
+  document.getElementById('but2').innerText = 'Mês de Janeiro PAGO!'
+}
+function carConteudo3() {
+  document.getElementById('but3').innerText = 'Mês de Fevereiro PAGO!'
+}
+function carConteudo4() {
+  document.getElementById('but4').innerText = 'Mês de Março PAGO!'
+}
+function carConteudo5() {
+  document.getElementById('but5').innerText = 'Mês de Abril PAGO!'
+}
+function carConteudo6() {
+  document.getElementById('but6').innerText = 'Mês de Maio PAGO!'
+}
+function carConteudo7() {
+  document.getElementById('but7').innerText = 'Mês de Junho PAGO!'
+}
+function carConteudo8() {
+  document.getElementById('but8').innerText = 'Mês de Julho PAGO!'
+}
+function carConteudo9() {
+  document.getElementById('but9').innerText = 'Mês de Agosto PAGO!'
+}
+function carConteudo10() {
+  document.getElementById('but10').innerText = 'Mês de Setembro PAGO!'
+}
+function carConteudo11() {
+  document.getElementById('but11').innerText = 'Mês de Outubro PAGO'
+}
+function carConteudo12() {
+  document.getElementById('but12').innerText = 'Mês de Novembro PAGO!'
+}
